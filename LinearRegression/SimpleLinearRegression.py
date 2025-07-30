d93d5acdd9ede4b6f0e3ee146a0c3e6d1884a815
parnika0724 <parnika03066030@gmail.com>
2025-07-31 01:37:51 +0530

    Update

--- LinearRegression/SimpleLinearRegression.py
+++ LinearRegression/SimpleLinearRegression.py
@@ -1,11 +1,11 @@
-# import matplotlib.pyplot as plt 
-# import numpy as np 
-# import pandas as pd 
-# a = np.array(4)
-# print(a)
-# print(a.ndim)
-# arr = np.array([1,2,3,4,5,6])
-# print(arr)
-# print(arr.ndim)
+import matplotlib.pyplot as plt 
+import numpy as np 
+import pandas as pd 
+a = np.array(4)
+print(a)
+print(a.ndim)
+arr = np.array([1,2,3,4,5,6])
+print(arr)
+print(arr.ndim)
 
 
